98ceb501861545f011ff5b1e47e93449f485cdf0
JamesPinkston <james.pinkston@outlook.com>
2024-11-19 20:46:20 -0600

    first commit

---
+++ dashboard/app.py
@@ -0,0 +1,111 @@
+# Import packages from pyshiny express and the python standard library
+from shiny import reactive, render
+from shiny.express import ui
+import random
+from datetime import datetime
+from faicons import icon_svg
+
+# If working locally, add the following to requirements.txt
+# faicons
+# shiny
+# shinylive
+
+# --------------------------------------------
+# SET UP THE REACTIVE CONTENT
+# --------------------------------------------
+
+# --------------------------------------------
+# PLANNING: We want to get a fake temperature and 
+# Time stamp every N seconds. 
+# For now, we'll avoid storage and just 
+# Try to get the fake live data working and sketch our app. 
+# We can do all that with one reactive calc.
+# Use constants for update interval so it's easy to modify.
+# ---------------------------------------------------------
+
+# --------------------------------------------
+# First, set a constant UPDATE INTERVAL for all live data
+# Constants are usually defined in uppercase letters
+# Use a type hint to make it clear that it's an integer (: int)
+# --------------------------------------------
+UPDATE_INTERVAL_SECS: int = 1
+# --------------------------------------------
+
+# Initialize a REACTIVE CALC that our display components can call
+# to get the latest data and display it.
+# The calculation is invalidated every UPDATE_INTERVAL_SECS
+# to trigger updates.
+
+# It returns everything needed to display the data.
+# Very easy to expand or modify.
+# (I originally looked at REACTIVE POLL, but this seems to work better.)
+# --------------------------------------------
+
+@reactive.calc()
+def reactive_calc_combined():
+
+    # Invalidate this calculation every UPDATE_INTERVAL_SECS to trigger updates
+    reactive.invalidate_later(UPDATE_INTERVAL_SECS)
+
+    # Data generation logic. Get random between -18 and -16 C, rounded to 1 decimal place
+    temp = round(random.uniform(-18, -16), 1)
+
+    # Get a timestamp for "now" and use string format strftime() method to format it
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    latest_dictionary_entry = {"temp": temp, "timestamp": timestamp}
+
+    # Return everything we need
+    return latest_dictionary_entry
+
+# ------------------------------------------------
+# Define the Shiny UI Page layout - Page Options
+# ------------------------------------------------
+
+# Call the ui.page_opts() function
+# Set title to a string in quotes that will appear at the top
+# Set fillable to True to use the whole page width for the UI
+
+ui.page_opts(title="PyShiny Express: Live Data (Basic)", fillable=True)
+
+# ------------------------------------------------
+# Define the Shiny UI Page layout - Sidebar
+# ------------------------------------------------
+
+# Sidebar is typically used for user interaction/information
+# Note the with statement to create the sidebar followed by a colon
+# Everything in the sidebar is indented consistently
+
+with ui.sidebar(open="open"):
+    ui.h2("Antarctic Explorer", class_="text-center")
+    ui.p(
+        "A demonstration of real-time temperature readings in Antarctica.",
+        class_="text-center",
+    )
+
+#---------------------------------------------------------------------
+# In Shiny Express, everything not in the sidebar is in the main panel
+#---------------------------------------------------------------------
+
+
+ui.h2("Current Temperature")
+
+@render.text
+def display_temp():
+    """Get the latest reading and return a temperature string"""
+    latest_dictionary_entry = reactive_calc_combined()
+    return f"{latest_dictionary_entry['temp']} C"
+
+ui.p("warmer than usual")
+
+icon_svg("sun")
+
+ui.hr()
+
+ui.h2("Current Date and Time")
+
+@render.text
+def display_time():
+    """Get the latest reading and return a timestamp string"""
+    latest_dictionary_entry = reactive_calc_combined()
+    return f"{latest_dictionary_entry['timestamp']}"
